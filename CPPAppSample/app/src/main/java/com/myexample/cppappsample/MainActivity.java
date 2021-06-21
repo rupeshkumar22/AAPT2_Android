a5d28c8d357f8d522497811903537d1d8d6e0b0f
rupesh-kumar-lpu <rk1307825@gmail.com>
2021-06-22 04:54:36 +0530

    First commit

---
+++ CPPAppSample/app/src/main/java/com/myexample/cppappsample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.myexample.cppappsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        //System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
